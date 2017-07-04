927cef35e23a955d14d74c72130c2616524234ad
Tobias Machulski <yabbaspost@gmail.com>
2017-07-04 20:22:17 +0200

    added teamName

--- foot/models.py
+++ foot/models.py
@@ -4,7 +4,7 @@ import urllib.request
 
 # Create your models here.
 
-
+"""
 class Standings():
     en_source = 'http://api.football-data.org/v1/soccerseasons/426/leagueTable'
     table = urllib.request.urlopen(en_source)
@@ -13,3 +13,23 @@ class Standings():
 
     def __str__(self):
         return "en_standings"
+"""
+
+
+class StandingsNew():
+    def __init__(self, country):
+        self.country = country
+        self.sources = {
+            'en': 'http://api.football-data.org/v1/soccerseasons/426/leagueTable',
+            'de': 'http://api.football-data.org/v1/soccerseasons/430/leagueTable',
+            'it': 'http://api.football-data.org/v1/soccerseasons/438/leagueTable',
+            'fr': 'http://api.football-data.org/v1/soccerseasons/434/leagueTable',
+            'es': 'http://api.football-data.org/v1/soccerseasons/436/leagueTable'
+        }
+
+    def get_standings(self):
+        country_source = self.sources[self.country]
+        table = urllib.request.urlopen(country_source)
+        table_str = table.read().decode('utf-8')
+        table_obj = json.loads(table_str)
+        return table_obj['standing']
--- foot/urls.py
+++ foot/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^en/', views.en, name="en"),
     url(r'^de/', views.de, name="de"),
     url(r'^fr/', views.fr, name="fr"),
+    url(r'^es/', views.es, name="es"),
+    url(r'^it/', views.it, name="it"),
     url(r'^$', views.main, name="main")
 ]
